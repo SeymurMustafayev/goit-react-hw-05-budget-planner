0c15e8ede764240a6bbeea2be064e1124204fd28
Seymur <mustafav.seymur@gmail.com>
2019-12-16 14:05:49 +0200

    Updates

---
+++ precache-manifest.66155092bc6cfbc57426c560a2229a99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451c0d9f42a5480ded2dc6452d038052",
+    "url": "/goit-react-hw-05-budget-planner/./index.html"
+  },
+  {
+    "revision": "38ae521940e9a6c5d22c",
+    "url": "/goit-react-hw-05-budget-planner/./static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "18231035bc381025c5ad",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/2.2def5f0d.chunk.js"
+  },
+  {
+    "revision": "38ae521940e9a6c5d22c",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/main.c3634a3e.chunk.js"
+  },
+  {
+    "revision": "ee7f5f6ccf0b027b4b13",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/runtime-main.3c5613c8.js"
+  }
+]);
